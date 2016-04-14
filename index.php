9e03de64d85736f6239e1dc27c3941c7bbd42ae4
Jared Meit <jared@modestcraft.com>
2016-04-14 00:56:59 -0400

    approved product with auto-populating video, and flash fallback for IE8

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once('video_list.php'); global $videos; ?>
 <!doctype html>
 <html class="no-js" lang="">
     <head>
@@ -11,8 +12,12 @@
 		
 		<link href='https://fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'>
 		
+		<link rel="stylesheet" href="//releases.flowplayer.org/6.0.5/skin/minimalist.css">
+		
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
+		
+		<script src="js/vendor/flowplayer_desktop.js"></script>
     </head>
     <body>
         <!--[if lt IE 8]>
@@ -27,16 +32,48 @@
 			$numBoxes = 7;
 			
 			
-			for($i=0;$i<$numBoxes;$i++){
+			// for($i=0;$i<$numBoxes;$i++){
+			foreach($videos as $i => $vid){
 				?>
 				<div class="player-module">
-					<h2 class="vid-title">Title for video <?=rand(10000,99999)?></h2>
-					<div class="player"></div>
+					<h2 class="vid-title"><?=$vid['title']?><br /><?=$vid['meta']?></h2>
+					<div class="player">
+						<video preload="metadata" controls poster="<?=$vid['poster']?>" >
+							<?php
+							foreach($vid['sources'] as $src){
+								?>
+								<source type="<?=$src['type']?>" src="<?=$src['src']?>" />
+								<?php
+							}
+							
+							/*
+							* Fallback to Flowplayer
+							*/
+							?>
+							<a href="https://s3.amazonaws.com/l39gg632kcb0ewpfj-sites-default-files/2016_Videos/ST_Feb5_VAN_01_Singh.mp4"
+								background="https://www.oncologyeducation.com/assets/images/Speaker%20Tours/pNETS%202016/SpeakerTourJan282016_01_Singh.jpg"
+							    style="display:block;width:384px;height:216px;"
+							    id="player<?=$i?>">
+							</a>
+						 
+							<script language="JavaScript">
+							  flowplayer("player<?=$i?>", "js/vendor/flowplayer-3.2.18.swf");
+							</script>
+						</video>
+					</div>
 				</div>
 				<?php
 			}
 			?>
 			
 		</div>
+		
+		
+		<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+		<script src="js/main.js"></script>
+		
+		
+		
     </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+
+/* 
+var w = window,
+    d = document,
+    e;
+w._fpes || (w._fpes = [], w.addEventListener("load", function() {
+    var s = d.createElement("script");
+    s.src = "//embed.flowplayer.org/6.0.5/embed.min.js", d.body.appendChild(s)
+// })), e = [].slice.call(d.getElementsByTagName("script"), -1)[0].parentNode, w._fpes.push({
+})), e = d.querySelector("#test-vid .player"), w._fpes.push({
+    e: e,
+    l: "",
+    c: {
+        "ratio": 0.5625,
+        "rtmp": 0,
+        "live": false,
+        "origin": "https://flowplayer.org/standalone/quickstart.html",
+        "swf": "//releases.flowplayer.org/6.0.5/flowplayer.swf",
+        "swfHls": "//releases.flowplayer.org/6.0.5/flowplayerhls.swf",
+        "adaptiveRatio": false,
+        "clip": {
+            "subtitles": [],
+            "sources": [{
+                "src": "http://l39gg632kcb0ewpfj-sites-default-files.s3.amazonaws.com/2015%20Webinars/MDS_Storring_May13_2015.mp4",
+                "suffix": "webm",
+                "type": "video/webm"
+            }],
+            "src": "http://l39gg632kcb0ewpfj-sites-default-files.s3.amazonaws.com/2015%20Webinars/MDS_Storring_May13_2015.mp4",
+            "suffix": "webm",
+            "type": "video/webm",
+            "duration": 68.004,
+            "width": 384,
+            "height": 216,
+            "url": "http://l39gg632kcb0ewpfj-sites-default-files.s3.amazonaws.com/2015%20Webinars/MDS_Storring_May13_2015.mp4",
+            "seekable": 68.004,
+            "time": 0
+        }
+    }
+});
+ */
+
+
+/* 
+flowplayer("player", "flowplayer-3.2.18.swf",{
+	clip: {
+		scaling: "fit"
+	}
+});
+ */
